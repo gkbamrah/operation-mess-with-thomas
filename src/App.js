5cdc8c05cb0395df7b7e0a068ee70bfa9cf58925
Guramrit Bamrah <guramritb@gmail.com>
2023-02-14 00:55:03 -0800

    moved everything up a level

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Cipher from "./components/Cipher";
+
+function App() {
+  return (
+    <div className="App">
+      <Cipher />
+    </div>
+  );
+}
+
+
+
+export default App;
